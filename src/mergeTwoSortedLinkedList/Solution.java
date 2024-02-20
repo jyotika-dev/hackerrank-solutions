90e917c547940da51d888139b193a2cf53176e73
Jyotika Kishor <jkishor1802@gmail.com>
2024-02-20 19:43:48 +0530

    Merge two sorted linked lists

--- src/mergeTwoSortedLinkedList/Solution.java
+++ src/mergeTwoSortedLinkedList/Solution.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Solution {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
         while (node != null) {
@@ -62,30 +62,30 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int tests = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int tests = sc.nextInt();
+        sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         for (int testsItr = 0; testsItr < tests; testsItr++) {
             SinglyLinkedList llist1 = new SinglyLinkedList();
 
-            int llist1Count = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            int llist1Count = sc.nextInt();
+            sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
             for (int i = 0; i < llist1Count; i++) {
-                int llist1Item = scanner.nextInt();
-                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+                int llist1Item = sc.nextInt();
+                sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
                 llist1.insertNode(llist1Item);
             }
 
             SinglyLinkedList llist2 = new SinglyLinkedList();
 
-            int llist2Count = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            int llist2Count = sc.nextInt();
+            sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
             for (int i = 0; i < llist2Count; i++) {
-                int llist2Item = scanner.nextInt();
-                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+                int llist2Item = sc.nextInt();
+                sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
                 llist2.insertNode(llist2Item);
             }
@@ -98,7 +98,7 @@ public class Solution {
 
         bufferedWriter.close();
 
-        scanner.close();
+        sc.close();
     }
 
     static class SinglyLinkedListNode {
